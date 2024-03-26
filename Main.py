c534ee1318d41a3a6d60e6ab24b55fce4e50dcc2
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-03-27 00:18:41 +0100

    Updating nbrs

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ import pygame
 import math
 from queue import PriorityQueue
 
-Width = 800
-WIN = pygame.display.set_mode((Width, Width))
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("Shortest Path Finder")
 
 RED = (255, 0, 0)
@@ -47,7 +47,7 @@ class Spot:
         return self.color == TURQUOISE
     
     def reset(self):
-        self.color == WHITE
+        self.color = WHITE
 
     def make_start(self):
         self.color = ORANGE
@@ -71,7 +71,18 @@ class Spot:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
     def update_neighbors(self, grid):
-        pass
+        self.neighbors = []
+        if self.row < self.total_rows - 1 and not grid[self.row - 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row + 1][self.col])
+
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row - 1][self.col])
+
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col + 1])
+
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col + 1])
 
     def __lt__(self, ohter):
         return False
@@ -128,6 +139,7 @@ def main(win, width):
     run = True
     started = False
     while run:
+        draw(win, grid, ROWS, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -139,11 +151,11 @@ def main(win, width):
                 pos = pygame.mouse.get_pos()
                 row, col = get_clicked_pos(pos, ROWS, width)
                 spot = grid[row][col]
-                if not start:
+                if not start and spot != end:
                     start = spot
                     start.make_start()
 
-                elif not end:
+                elif not end and spot != start:
                     end = spot
                     end.make_end()
 
@@ -151,11 +163,30 @@ def main(win, width):
                     spot.make_barrier()
 
             elif pygame.mouse.get_pressed()[2]:
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                spot.reset()
+                if spot == start:
+                    start = None
+                
+                if spot == end:
+                     end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors()
+                    
+                    algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
+                    x = lambda: print("hey")
+                    x()
+
 
     pygame.quit()
 
-main()
+main(WIN, WIDTH)
 
 
 
