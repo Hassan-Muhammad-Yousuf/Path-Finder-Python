6f225316ec1bc7d93be76bb0ef74c5b3f72bbe2e
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-03-27 10:33:48 +0100

    A* Algorithm Progress

--- Main.py
+++ Main.py
@@ -120,8 +120,20 @@ def algorithm(draw, grid, start, end):
 
             if temp_g_score < g_score[neighbor]:
                came_from[neighbor] = current
-               g_score[current] = temp_g_score
-               f_score[current] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+               g_score[neighbor] = temp_g_score
+               f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+               if neighbor not in open_set_hash:
+                   count += 1
+                   open_set.put((f_score[neighbor], count, neighbor))
+                   open_set_hash.add(neighbor)
+                   neighbor.make_open()
+
+        draw()
+        if current != start:
+            current.make_closed()
+
+    return False
+                   
 
 def make_grid(rows, width):
     grid = []
